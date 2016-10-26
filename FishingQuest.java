ddc08fee2e4b00c17ca0e3626f951cb865ab549f
Sacredwisp <mmbradetlegris@mta.ca>
2016-10-26 16:12:41 -0300

    QuestUpdate Michael

--- FishingQuest.java
+++ FishingQuest.java
@@ -1,20 +1,20 @@
 public class FishingQuest extends Quest {
 
-	private static final int WORKCRED_COST = 20;
+	protected static final int WORKCRED_COST = 20;
 
-	private static final double FISH_CHANCE = 0.7;
-	private static final double FISH_IS_BIG_CHANCE = 0.3;
-	private static final double NESSY_CHANCE = 0.03;
-	private static final long REGULAR_FISH_MAX_STREETCRED_MULTIPLIER = 100;
-	private static final long BIG_FISH_MAX_STREETCRED_MULTIPLIER = 500;
-	private static final long NESSY_MAX_STREETCRED_MULTIPLIER = 10000;
+	protected static final double FISH_CHANCE = 0.7;
+	protected static final double FISH_IS_BIG_CHANCE = 0.3;
+	protected static final double NESSY_CHANCE = 0.03;
+	protected static final long REGULAR_FISH_MAX_STREETCRED_MULTIPLIER = 100;
+	protected static final long BIG_FISH_MAX_STREETCRED_MULTIPLIER = 500;
+	protected static final long NESSY_MAX_STREETCRED_MULTIPLIER = 10000;
 
 	/**
-	 * Constructor. Generates a quest with the specified skill level.
+	 * Constructor. Generates a quest with assumed skill level of 1.
 	 */
-	public FishingQuest(int skillLevel) {
-		super(skillLevel);
-		super.workCredCost = WORKCRED_COST;
+	public FishingQuest() {
+		super();
+		workCredCost = WORKCRED_COST;
 	}
 
 	public void generateRandomEvents(int skillLevel) {
@@ -31,13 +31,13 @@ public class FishingQuest extends Quest {
 	 * caught. Number of fish caught is partially random, but increases if the
 	 * multiplier increases. Payoffs are partially random, but increase if more
 	 * fish are caught and if the player has a higher skill level.
-	 *
+	 * 
 	 * @param skillLevel
 	 *            player's skill level.
 	 * @param multiplier
 	 *            increases chance of catching fish.
 	 */
-	public void fish(int skillLevel, int multiplier) {
+	protected int[] fish(int skillLevel, int multiplier) {
 		int numRegFishes = 0;
 		int numBigFishes = 0;
 		int chancesLeft = multiplier;
@@ -52,18 +52,21 @@ public class FishingQuest extends Quest {
 		}
 		if (numRegFishes == 0 && numBigFishes == 0) {
 			infoString += "Oh no! Looks like today was bad for fishing! \n";
-			return;
+			int[] fish = {numRegFishes, numBigFishes};
+			return fish;
 		}
 		infoString += "Pulled some nice fish! " + numRegFishes
 				+ " regular fish and " + numBigFishes + "big fish!\n";
 
-		streetCredGain += random.nextLong()
-				% (numRegFishes * REGULAR_FISH_MAX_STREETCRED_MULTIPLIER * skillLevel);
-		streetCredGain += random.nextLong()
-				% (numBigFishes * BIG_FISH_MAX_STREETCRED_MULTIPLIER * skillLevel);
+		if (numRegFishes > 0)
+			streetCredGain += Math.abs(random.nextLong()) % (numRegFishes * REGULAR_FISH_MAX_STREETCRED_MULTIPLIER * skillLevel);
+		if (numBigFishes > 0)
+			streetCredGain += Math.abs(random.nextLong()) % (numBigFishes * BIG_FISH_MAX_STREETCRED_MULTIPLIER * skillLevel);
 
 		infoString += "Got meself a hefty " + streetCredGain
 				+ " streetCred by selling 'em!\n";
+		int[] fish = {numRegFishes, numBigFishes};
+		return fish;
 	}
 
 	/**
@@ -71,25 +74,27 @@ public class FishingQuest extends Quest {
 	 * Loch Ness Monster.  The payoff is partially random but also increases with
 	 * the user's skillLevel.  The chance to catch Nessy is random, but increases
 	 * with the multiplier.
-	 *
+	 * 
 	 * @param skillLevel
 	 *            player's skill level.
 	 * @param multiplier
 	 *            increases chance of catching Nessy.
 	 */
-	public void nessy(int skillLevel, int multiplier) {
+	protected boolean nessy(int skillLevel, int multiplier) {
 		int chances = multiplier;
 		while (chances > 0){
 			if (random.nextDouble() <= NESSY_CHANCE) {
-				long bonus = random.nextLong()
+				long bonus = Math.abs(random.nextLong())
 						% (skillLevel * NESSY_MAX_STREETCRED_MULTIPLIER);
 				infoString += "Woah! Looks like I pulled Nessy! Bonus of " + bonus
 					+ " streetCred!\n";
 				streetCredGain += bonus;
+				return true;
 			}
 			else
 				chances--;
 		}
+		return false;
 	}
 
 	public boolean canPerformQuest(int currentWorkCred) {
