b45f6f6721abca0f3ef517449275fde7b9109721
Finn Lidbetter <tflidbetter@mta.ca>
2016-11-18 12:32:36 -0400

    Javadoc for decorator

--- FishingSkill.java
+++ FishingSkill.java
@@ -4,6 +4,16 @@
  * @version 2.0, 16/11/18
  */
 public interface FishingSkill {
+
+  /**
+   * getFishingSkill method returns the fishing skill level for the player
+   * @return the fishing skill level
+   */
   public int getFishingSkill();
+
+  /**
+   * getBestRod method returns the best fishing rod that this player has
+   * @return the best rod the player has
+   */
   public FishingRod getBestRod();
 }
